a2b9701c97ecd5fbafc48521a6f477f7543c3069
Ram Charan Reddy Dokur <dealzone6244@gmail.com>
2026-03-16 12:00:00 +0530

    Implemented coupon persistence and management in the repository using Firestore

--- app/src/main/java/uk/ac/tees/mad/dealzone/domain/Repo/Repo.kt
+++ app/src/main/java/uk/ac/tees/mad/dealzone/domain/Repo/Repo.kt
@@ -5,5 +5,8 @@ import uk.ac.tees.mad.dealzone.Model.ResultState
 import kotlinx.coroutines.flow.Flow
 
 interface Repo {
-fun getproducts(): Flow<ResultState<List<Product>>>
+    fun getproducts(): Flow<ResultState<List<Product>>>
+    fun getSavedCoupons(uid: String): Flow<ResultState<List<Product>>>
+    suspend fun saveCoupon(uid: String, product: Product): ResultState<Unit>
+    suspend fun removeCoupon(uid: String, productId: Int): ResultState<Unit>
 }
