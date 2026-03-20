f987a6c72e9f4cd0393d17c58f340dc3c9e2b272
Ram Charan Reddy Dokur <dealzone6244@gmail.com>
2026-03-20 12:00:00 +0530

    Implemented SavedCouponsViewModel and integrate SavedCouponsScreen into the navigation graph

--- app/src/main/java/uk/ac/tees/mad/dealzone/Model/Product.kt
+++ app/src/main/java/uk/ac/tees/mad/dealzone/Model/Product.kt
@@ -3,14 +3,14 @@ package uk.ac.tees.mad.dealzone.Model
 import com.google.gson.annotations.SerializedName
 
 data class Product(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val price: Double,
+    val id: Int = 0,
+    val title: String = "",
+    val description: String = "",
+    val price: Double = 0.0,
     @SerializedName("discountPercentage")
-    val discount: Double,
-    val rating: Double,
-    val category: String,
+    val discount: Double = 0.0,
+    val rating: Double = 0.0,
+    val category: String = "",
     @SerializedName("thumbnail")
-    val image: String
+    val image: String = ""
 )
